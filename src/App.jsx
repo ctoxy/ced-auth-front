a98258909495144533d3eb8bcaa1cea7d5a572e6
ctoxy <cedric.bouve@gmail.com>
2021-05-28 11:13:10 +0200

    nested component maincontent

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import Layout from './core/Layout';
 import { Helmet } from 'react-helmet';
+import MainContent from './component/Maincontent';
 
 const App = () => {
     const head = () => (
@@ -13,18 +14,7 @@ const App = () => {
     return (
         <Layout>
             {head()}
-            <div className="col-md-6 offset-md-3 text-center">
-                <h1 className="p-5">React Node MongoDB Authentication Boilerplate</h1>
-                <h2>MERN STACK</h2>
-                <hr />
-                <p className="lead">
-                    MERN stack login register system with account activation, forgot password, reset password, login
-                    with facebook and google as well as private and protected routes for authenticated user and users
-                    with the role of admin.
-                </p>
-                <h4 className="p2">Bonus</h4>
-                <p className="lead">Profile update & deployment to digital ocean cloud servers</p>
-            </div>
+            <MainContent />
         </Layout>
     );
 };
---
+++ src/component/Maincontent.jsx
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+
+export default class MainContent extends Component {
+    render() {
+        return (
+            <div className="col-md-6 offset-md-3 text-center">
+                <h1 className="p-5">React Node MongoDB Authentication Boilerplate</h1>
+                <h2>MERN STACK</h2>
+                <hr />
+                <p className="lead">
+                    MERN stack login register system with account activation, forgot password, reset password, login
+                    with facebook and google as well as private and protected routes for authenticated user and users
+                    with the role of admin.
+                </p>
+                <h4 className="p2">Bonus</h4>
+                <p className="lead">Profile update & deployment to digital ocean cloud servers</p>
+            </div>
+        );
+    }
+}
