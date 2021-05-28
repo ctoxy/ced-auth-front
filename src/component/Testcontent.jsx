5a5c83c37bbedd22d8a58a59bc0d33fd2b8a2814
ctoxy <cedric.bouve@gmail.com>
2021-05-28 14:23:43 +0200

    pass events argument on click button photo

--- src/component/Testcontent.jsx
+++ src/component/Testcontent.jsx
@@ -43,13 +43,6 @@ export default class TestContent extends Component {
             }
         ]
     };
-
-    customerNameStyle = (custName) => {
-        if (custName.startsWith('S')) { return "green-highlight border-left" }
-        else if (custName.startsWith('J')) { return "red-highlight border-left" }
-        else { return {}; }
-    };
-
     /*execute when a user click on */
     onRefreshClick = () => {
         this.setState({
@@ -65,19 +58,43 @@ export default class TestContent extends Component {
     };
 
     getCustomerRow = () => {
-        return this.state.customers.map((cust) => {
+        return this.state.customers.map((cust, index) => {
             return (
                 <tr key={cust.id}>
                     <td>{cust.id}</td>
                     <td>
                         <img src={cust.photo} alt="Customer" />
+                        <div>
+                            <button
+                                className="btn btn-sm btn-primary"
+                                onClick={() => {
+                                    this.onChangePictureClick(cust, index);
+                                }}
+                            >
+                                Change Picture
+                  </button>
+                        </div>
                     </td>
-                    <td className={this.customerNameStyle(cust.name)}>{cust.name}</td>
+                    <td>{cust.name}</td>
                     <td>{this.getPhoneToRender(cust.phone)}</td>
                     <td>{cust.address.city}</td>
                 </tr>
             );
         });
+    };
+
+    //Executes when the user clicks on "Change Picture" button in the grid
+    //Receives the "customer" object and index of the currently clicked customer
+    onChangePictureClick = (cust, index) => {
+        //console.log(cust);
+        //console.log(index);
+
+        //get existing customers
+        var custArr = this.state.customers;
+        custArr[index].photo = "https://picsum.photos/id/104/60";
+
+        //update "customers" array in the state
+        this.setState({ customers: custArr });
     };
     render() {
         return (
