16288eb1a4b6708a776dd9b64ee72a24b68b5364
ctoxy <cedric.bouve@gmail.com>
2021-05-28 10:13:08 +0200

    import bootstrap jquery poper via npm

--- src/index.js
+++ src/index.js
@@ -1,6 +1,10 @@
 import React from 'react';
 import { hydrate, render } from 'react-dom';
 import Routes from './Routes';
+import 'jquery';
+import 'popper.js/dist/umd/popper';
+import 'bootstrap/dist/js/bootstrap';
+import 'bootstrap/dist/css/bootstrap.css';
 
 const rootElement = document.getElementById('root');
 if (rootElement.hasChildNodes()) {
